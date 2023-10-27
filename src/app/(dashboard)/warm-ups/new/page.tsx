4694c295d78ba638f19a8eae8323a72d3eb1857f
mikeyv <michaelvik520@gmail.com>
2023-10-27 16:29:57 +0100

    fix: fix typescript issues

--- src/app/(dashboard)/warm-ups/new/page.tsx
+++ src/app/(dashboard)/warm-ups/new/page.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 import TextInput from '@/app/(dashboard)/components/TextInput'
 
 const CreateWarmup = () => {
+
+  function handleOnChange(){
+  }
+
   return (
     <section className="w-full h-full overflow-auto flex bg-gray-150 px-5 py-10">
       <div className="w-full h-fit flex flex-col items-center just50y-center gap-5 ">
@@ -12,19 +16,19 @@ const CreateWarmup = () => {
           </div>
           <div className="flex gap-6 h-full">
             <div className="w-full flex flex-col gap-5">
-              <TextInput label='Select Mail Server Associated with this Warmup' placeholder='Select A "Mail Server" List' />
-              <TextInput label='Select Client Email Associated with this Warmup' placeholder='Select A Client Email List' />
-              <TextInput label="Warm-up Name" placeholder="Enter your warm-up name" />
-              <TextInput label="Warm-up Name" placeholder="Enter your warm-up name" />
-              <TextInput label="Warm-up Name" placeholder="Enter your warm-up name" />
+              <TextInput label='Select Mail Server Associated with this Warmup' placeholder='Select A "Mail Server" List' value="" onChange={handleOnChange} />
+              <TextInput label='Select Client Email Associated with this Warmup' placeholder='Select A Client Email List' value="" onChange={handleOnChange} />
+              <TextInput label="Warm-up Name" placeholder="Enter your warm-up name" value="" onChange={handleOnChange} />
+              <TextInput label="Warm-up Name" placeholder="Enter your warm-up name"  value="" onChange={handleOnChange}/>
+              <TextInput label="Warm-up Name" placeholder="Enter your warm-up name" value="" onChange={handleOnChange} />
               <button type="button" className="px-5 md:px-10 py-2 bg-blue rounded-md text-white w-fit">Submit</button>
             </div>
 
             <div className="w-full flex flex-col gap-5">
-              <TextInput label='Select Mail Server Associated with this Warmup' placeholder='Select A "Mail Server" List' />
-              <TextInput label="Warm-up Name" placeholder="Enter your warm-up name" />
-              <TextInput label="Warm-up Name" placeholder="Enter your warm-up name" />
-              <TextInput label="Warm-up Name" placeholder="Enter your warm-up name" />
+              <TextInput label='Select Mail Server Associated with this Warmup' placeholder='Select A "Mail Server" List' value="" onChange={handleOnChange}/>
+              <TextInput label="Warm-up Name" placeholder="Enter your warm-up name" value="" onChange={handleOnChange} />
+              <TextInput label="Warm-up Name" placeholder="Enter your warm-up name" value="" onChange={handleOnChange} />
+              <TextInput label="Warm-up Name" placeholder="Enter your warm-up name" value="" onChange={handleOnChange} />
             </div>
           </div>
         </form>
